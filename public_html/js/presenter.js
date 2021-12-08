29ad4afcc8e7998948f8f35e5a6eb200d9dce544
WebAppWE1 <s84534@beuth-hochschule.de>
2021-12-08 16:29:08 +0100

    small adjustments view-components

--- public_html/js/presenter.js
+++ public_html/js/presenter.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,78 +6,86 @@
 
 "use strict";
 const presenter = (function () {
-    // Private Variablen und Funktionen
-    let init = false;
-    let owner = null;
-    let blogId = 0;
+  // Private Variablen und Funktionen
+  let init = false;
+  let owner = null;
+  let blogId = 0;
 
-    // Initialisiert die allgemeinen Teile der Seite
-    function initPage() {
-        console.log("Presenter: Aufruf von initPage()");
+  // Initialisiert die allgemeinen Teile der Seite
+  function initPage() {
+    console.log("Presenter: Aufruf von initPage()");
 
-        model.getAllBlogs((blogs) => {
-            console.log('Blog-Overview wird geladen...');
-            let element = blogOverview.render(blogs);
-            replace("blog-overview", element);
+    model.getAllBlogs((blogs) => {
+      console.log("Blog-Overview wird geladen...");
+      let element = blogOverview.render(blogs);
+      replace("blog-overview", element);
 
-            blogId = blogs[0].id;
-            model.getBlog(blogId, (blog) => {
-                console.log('Blog-detail-info wird aufgerufen...');
-                blog.setFormatDates(true);
-                let element = blogInfo.render(blog);
-                replace("blog-detail-info", element)
-            });
+      blogId = blogs[0].id;
+      model.getBlog(blogId, (blog) => {
+        console.log("Blog-detail-info wird aufgerufen...");
+        blog.setFormatDates(true);
+        let element = blogInfo.render(blog);
+        replace("blog-detail-info", element);
+      });
 
-            //Falls auf Startseite, navigieren zu Uebersicht
-            if (window.location.pathname === "/")
-            router.navigateToPage('/blogOverview/' + blogId);
-        });
-    
-        // Nutzer abfragen und Anzeigenamen als owner setzen
-        model.getSelf((result) => {
-            owner = result.displayName;
-            console.log(`Presenter: Nutzer*in ${owner} hat sich angemeldet.`);
-        });
-        
-        // Das muss später an geeigneter Stelle in Ihren Code hinein.
-        init = true;
-    }
-    // Sorgt dafür, dass bei einem nicht-angemeldeten Nutzer nur noch der Name der Anwendung
-    // und der Login-Button angezeigt wird.
-    function loginPage() {
-        console.log("Presenter: Aufruf von loginPage()");
-        if(owner !== undefined) console.log(`Presenter: Nutzer*in ${owner} hat sich abgemeldet.`);
-        init = false;
-        replace('blog-overview');
-        replace('blog-detail-info');
-    }
-    // Tauscht Templates in Bereichen aus, die durch die id-Wert bestimmt werden
-    function replace(id, newContent) {
-        let section = document.getElementById(id);
-        let oldContent = section.firstElementChild;
-        if (oldContent) oldContent.remove();
-        if (newContent) section.append(newContent);
-    }
+      model.getAllPostsOfBlog(blogId, (posts) => {
+          console.log("Post-overview wird aufgerufen...");
+          let element = postOverview.render(posts);
+          replace("main-section", element);
+      });
 
+      //Falls auf Startseite, navigieren zu Uebersicht
+      if (window.location.pathname === "/")
+        router.navigateToPage("/blogOverview/" + blogId);
+    });
 
-    //Oeffentliche Methoden
-    return {
-        // Wird vom Router aufgerufen, wenn die Startseite betreten wird
-        showStartPage() {
-            console.log("Aufruf von presenter.showStartPage()");
-            // Wenn vorher noch nichts angezeigt wurde, d.h. beim Einloggen
-            if (model.isLoggedIn()) { // Wenn der Nutzer eingeloggt ist
-                initPage();
-            }
-            if (!model.isLoggedIn()) { // Wenn der Nuzter eingelogged war und sich abgemeldet hat
-                //Hier wird die Seite ohne Inhalt angezeigt
-                loginPage();
-            }
-        },
+    // Nutzer abfragen und Anzeigenamen als owner setzen
+    model.getSelf((result) => {
+      owner = result.displayName;
+      console.log(`Presenter: Nutzer*in ${owner} hat sich angemeldet.`);
+    });
 
-        // Wird vom Router aufgerufen, wenn eine Blog-Übersicht angezeigt werden soll
-        showBlogOverview(bid) {
-           console.log(`Aufruf von presenter.showBlogOverview(${blogId})`); 
-        }
-    };
+    // Das muss später an geeigneter Stelle in Ihren Code hinein.
+    init = true;
+  }
+  // Sorgt dafür, dass bei einem nicht-angemeldeten Nutzer nur noch der Name der Anwendung
+  // und der Login-Button angezeigt wird.
+  function loginPage() {
+    console.log("Presenter: Aufruf von loginPage()");
+    if (owner !== undefined)
+      console.log(`Presenter: Nutzer*in ${owner} hat sich abgemeldet.`);
+    replace("blog-overview");
+    replace("blog-detail-info");
+    init = false;
+  }
+  // Tauscht Templates in Bereichen aus, die durch die id-Wert bestimmt werden
+  function replace(id, newContent) {
+    let section = document.getElementById(id);
+    let oldContent = section.firstElementChild;
+    if (oldContent) oldContent.remove();
+    if (newContent) section.append(newContent);
+  }
+
+  //Oeffentliche Methoden
+  return {
+    // Wird vom Router aufgerufen, wenn die Startseite betreten wird
+    showStartPage() {
+      console.log("Aufruf von presenter.showStartPage()");
+      // Wenn vorher noch nichts angezeigt wurde, d.h. beim Einloggen
+      if (model.isLoggedIn()) {
+        // Wenn der Nutzer eingeloggt ist
+        initPage();
+      }
+      if (!model.isLoggedIn()) {
+        // Wenn der Nuzter eingelogged war und sich abgemeldet hat
+        //Hier wird die Seite ohne Inhalt angezeigt
+        loginPage();
+      }
+    },
+
+    // Wird vom Router aufgerufen, wenn eine Blog-Übersicht angezeigt werden soll
+    showBlogOverview(bid) {
+      console.log(`Aufruf von presenter.showBlogOverview(${blogId})`);
+    },
+  };
 })();
--- public_html/js/view.js
+++ public_html/js/view.js
@@ -1,68 +1,84 @@
 /**
  * View-Komponenten der einzelnen Templates aus index.html mit den dazugehörigen render()-Funktionen
  */
-'use strict';
+"use strict";
 
-    const loggedIn = {
-      render(data) {
-        console.log("Model: render() von LoggedIn");
+const loggedIn = {
+  render(data) {
+    console.log("View: render() von LoggedIn");
 
-        let page = document
-          .getElementById("user-info-loggedIn-scheme")
-          .cloneNode(true);
-        page.removeAttribute("id");
-        helper.setDataInfo(page, data);
+    let page = document
+      .getElementById("user-info-loggedIn-scheme")
+      .cloneNode(true);
+    page.removeAttribute("id");
+    helper.setDataInfo(page, data);
 
-        return page;
-      },
-    };
+    return page;
+  },
+};
 
-    const blogOverview = {
-      render(data) {
-        console.log("Model: render() von blogOverview");
+const blogOverview = {
+  render(data) {
+    console.log("View: render() von blogOverview");
 
-        let page = document
-          .getElementById("blog-overview-scheme")
-          .cloneNode(true);
-        page.removeAttribute("id");
-        let list = page.querySelector("ul");
-        let listEleTemp = list.firstElementChild;
-        listEleTemp.remove();
-        for (let value of data) {
-          let listElement = listEleTemp.cloneNode(true);
-          list.appendChild(listElement);
-          helper.setDataInfo(list, value);
-        }
+    let page = document
+      .getElementById("blog-overview-scheme")
+      .cloneNode(true);
+    page.removeAttribute("id");
+    let list = page.querySelector("ul");
+    let listEleTemp = list.firstElementChild;
+    listEleTemp.remove();
+    for (let value of data) {
+      let listElement = listEleTemp.cloneNode(true);
+      list.appendChild(listElement);
+      helper.setDataInfo(list, value);
+    }
 
-        return page;
-      },
-    };
+    return page;
+  },
+};
 
-    const blogInfo = {
-      render(data) {
-        console.log("Model: render() von blogInfo");
+const blogInfo = {
+  render(data) {
+    console.log("View: render() von blogInfo");
 
-        let page = document
-            .getElementById("blog-info-scheme")
-            .cloneNode(true);
-        page.removeAttribute("id");
-        helper.setDataInfo(page, data);
+    let page = document
+      .getElementById("blog-info-scheme")
+      .cloneNode(true);
+    page.removeAttribute("id");
+    helper.setDataInfo(page, data);
 
-        return page;
-      },
-    };
+    return page;
+  },
+};
+
+const postOverview = {
+  render(data) {
+    console.log("View: render() von postOverview");
+
+    let page = document
+      .getElementById("post-overview-scheme")
+      .cloneNode(true);
+    page.removeAttribute("id");
+    let article = page.querySelector("article");
+    for (let value of data) {
+      helper.setDataInfo(article, value);
+    }
+
+    return page;
+  },
+};
 
 /**
  * helper-Funktion für das Einfügen von Values in die Seiten-Templates
  */
 const helper = {
-    
-    setDataInfo(element, object) {
-        let content = element.innerHTML;
-        for (let key in object) {
-            let rexp = new RegExp('%' + key, 'g');
-            content = content.replace(rexp, object[key]);
-        }
-        element.innerHTML = content;
+  setDataInfo(element, object) {
+    let content = element.innerHTML;
+    for (let key in object) {
+      let rexp = new RegExp("%" + key, "g");
+      content = content.replace(rexp, object[key]);
     }
+    element.innerHTML = content;
+  },
 };
